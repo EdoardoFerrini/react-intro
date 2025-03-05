3e002a139e47cd0ce9be5a2519c58bab7e330eb4
EdoardoFerrini <edoardo.ferrini@gmail.com>
2025-03-05 13:40:44 +0100

    Mouse Events

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,17 @@ import { Total } from "./components/Total";
 function App() {
   const totalProducts = 5;
 
-  return <>{totalProducts > 0 ? <Total value={totalProducts} /> : <Empty />}</>;
+  function inc(e: React.MouseEvent) {
+    console.log("inc", e.clientY);
+  }
+
+  return (
+    <>
+      {totalProducts > 0 ? <Total value={totalProducts} /> : <Empty />}
+
+      <button onClick={inc}>Add</button>
+    </>
+  );
 }
 
 export default App;
