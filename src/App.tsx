e78c7bd94e988c186dc13e9bb13040402d8b5d89
EdoardoFerrini <edoardo.ferrini@gmail.com>
2025-03-05 16:28:52 +0100

    useState & Map: aggiungere elementi alla lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-const users = [
+import { useState } from "react";
+
+const initialState = [
   { id: 1, name: "Fabio" },
   { id: 2, name: "Lorenzo" },
   { id: 3, name: "Silvia" },
 ];
 
 function App() {
+  const [users, setUsers] = useState(initialState);
+
+  function addUser() {
+    const newUser = { id: Date.now(), name: `user ${Math.random()}` };
+    setUsers((s) => [...s, newUser]);
+  }
+
   return (
     <>
       {users.map((user) => {
         return <li key={user.id}>{user.name}</li>;
       })}
+      <button onClick={addUser}>Add</button>
     </>
   );
 }
