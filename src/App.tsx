3ac644d55051d084e3a512867f5c2425676688c8
EdoardoFerrini <edoardo.ferrini@gmail.com>
2025-03-05 11:55:02 +0100

    React Components: Arrow Function vs Regular Function

--- src/App.tsx
+++ src/App.tsx
@@ -3,3 +3,21 @@ function App() {
 }
 
 export default App;
+
+/*
+
+const App = () => {
+  return <h1>Hello React</h1>;
+};
+
+export default App;
+
+*/
+
+/*
+
+const App = () => <div>Hello React</div>;
+
+export default App;
+
+*/
