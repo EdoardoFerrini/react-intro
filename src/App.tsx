a82dba39c2a26374df3b9c82dbdf989cb8d28db2
EdoardoFerrini <edoardo.ferrini@gmail.com>
2025-03-05 13:47:06 +0100

    Visualizzare un elenco di dati con map

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
-import { useState } from "react";
-import { Empty } from "./components/Empty";
-import { Total } from "./components/Total";
+const users = [
+  { id: 1, name: "Fabio" },
+  { id: 2, name: "Lorenzo" },
+  { id: 3, name: "Silvia" },
+];
 
 function App() {
-  const [totalProducts, setTotalProducts] = useState<number>(0);
-
-  function inc() {
-    setTotalProducts((s) => s + 1);
-  }
-
-  console.log("render", totalProducts);
-
   return (
     <>
-      {totalProducts > 0 ? <Total value={totalProducts} /> : <Empty />}
-
-      <button onClick={inc}>Add</button>
+      {users.map((user) => {
+        return <li key={user.id}>{user.name}</li>;
+      })}
     </>
   );
 }
