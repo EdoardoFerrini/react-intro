e942d48d9983c6619c755da92203c0dc5f49cfa1
EdoardoFerrini <edoardo.ferrini@gmail.com>
2025-03-05 11:58:18 +0100

    React Fragment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 function App() {
   return (
-    <div>
+    <>
       <h1>Hello</h1>
       <h1>Element 1</h1>
       <h1>Element 2</h1>
-    </div>
+    </>
   );
 }
 
 export default App;
+
+/*
+
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <h1>Hello</h1>
+      <h1>Element 1</h1>
+      <h1>Element 2</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
+
+*/
